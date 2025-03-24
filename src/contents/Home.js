ae01f45108c9b9126d3b44dc137b49a5f60d12dd
Thannatcha <s6603052413191@email.kmutnb.ac.th>
2025-03-24 18:18:28 +0700

    Update
    Update

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
             
             <div className="condiv home">
                 
-                <img src="https://scontent.fbkk22-2.fna.fbcdn.net/v/t39.30808-6/355622173_3513265168957147_4607387343439816604_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeE7DmhdmE1pu_VKuV8azbvfq7ckhFSGSYSrtySEVIZJhFgNp0ennoIHgqoMu3NVO4FiU37jAG6GOXn0wQUrepnq&_nc_ohc=p4odBv__D_cQ7kNvgHVSVYb&_nc_oc=AdjPIJ3C8Wh3VhcaFmaZF5Bpt3pJwrmbRu7uk8CY6d23LSpBpV6U48tRt5lwGbo-p4k&_nc_zt=23&_nc_ht=scontent.fbkk22-2.fna&_nc_gid=AyuPlC9HFzDxyLnA9JksE5v&oh=00_AYGD_yHNyR61H-GID5zq0VAbj42dWmbNLVDwW0vI_eYtxw&oe=67D39DC7" alt="profile" className="profilepic"/>
+                <img src="/image/than.jpg" alt="profile" className="profilepic"/>
                 <ReactTypingEffect text={['Hello','I am Thannatcha']} speed={70} eraseDelay={80} className="typingeffect" />
                 
             </div>
